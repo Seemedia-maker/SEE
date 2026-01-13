be3055690e2935a5da0f76dd093a94d84f616cb0
Seemedia-maker <service.see.contact@gmail.com>
2026-01-13 14:43:47 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,10 +5,27 @@ function login() {
 
   if (user === "admin" && pass === "see2026") {
     localStorage.setItem("connected", "true");
-    window.location.href = "dashboard.html"; // ✅ PAGE QUI DOIT EXISTER
+    window.location.href = "dashboard.html";
   } else {
     document.getElementById("error").innerText = "Accès refusé";
   }
+}
+
+/* PUBLISH */
+function publish() {
+  const article = {
+    title: document.getElementById("title").value,
+    image: document.getElementById("image").value,
+    source: document.getElementById("source").value,
+    content: document.getElementById("content").value,
+    date: new Date().toLocaleDateString()
+  };
+
+  const articles = JSON.parse(localStorage.getItem("articles")) || [];
+  articles.unshift(article);
+  localStorage.setItem("articles", JSON.stringify(articles));
+
+  window.location.href = "index.html";
 }
 
 /* LOAD ARTICLES */
@@ -20,9 +37,11 @@ if (container) {
     container.innerHTML += `
       <article class="card">
         <img src="${a.image}">
-        <h2>${a.title}</h2>
-        <p>${a.content}</p>
-        <span>${a.date}</span>
+        <div class="card-content">
+          <h2>${a.title}</h2>
+          <p>${a.content}</p>
+          <span>${a.source} · ${a.date}</span>
+        </div>
       </article>
     `;
   });
